71da733056f71eee506dea9e6a46fef21d41ed4f
matukai <matt.tanji@gmail.com>
2017-09-29 15:03:41 -1000

    fringos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var storeDiv = document.querySelectorAll('.name');
+// target parent divs ("name") and store them in array called "storeDiv"
 
+function reveal(){
+	var shopChildDiv = this.querySelector(".menu");
+	if(shopChildDiv.style.display === "none"){
+	shopChildDiv.style.display = "block";
+	}else{
+		shopChildDiv.style.display = "none";
+	}
+}
+
+for(var i = 0; i < storeDiv.length;  i++){
+	storeDiv[i].addEventListener("click",reveal);
+}
